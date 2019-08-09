c504948e42d9d5ac6fd274c6a13756024edd310f
ML1020 <1227797704@qq.com>
2019-08-09 20:15:29 +0800

    杨辉三角

---
+++ 08092.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int i, j, a[11][11] = {0};
+	for (i = 1; i < 11; i++)
+	{
+		a[i][i] = 1;
+		a[i][1] = 1;
+	}
+	for (i = 3; i < 11; i++)
+	for (j = 2; j <= i - 1; j++)
+		a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
+	for (i = 1; i < 11; i++)
+	{
+		for (j = 1; j <= i; j++)
+			printf("%4d", a[i][j]);
+		printf("\n");
+	}
+	system("pause");
+	return 0;
+}
